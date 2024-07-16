cbf0c0c1261405358d956a87eddf851439ff787a
LordPhantton <wcaio41@gmail.com>
2024-07-15 23:59:49 -0300

    create users and update prompt

--- configs/urls.py
+++ configs/urls.py
@@ -4,5 +4,7 @@ from chatbot import views
 urlpatterns = [
     path('', views.chat_view, name='chat_view'),
     path('send_message/', views.send_message, name='send_message'),
-    path("accounts/", include("accounts.urls"))
+    path('accounts/', include('accounts.urls')),
+    path('list-users/', views.list_users, name='list-users'),
+    path('create-user/', views.create_user, name='create-user'),
 ]
