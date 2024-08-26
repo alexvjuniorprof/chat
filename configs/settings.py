257f59096d487b14b2d1dac867be3a53efdabdc6
unknown <wcaio41@gmail.com>
2024-08-25 21:53:05 -0300

    Initial commit

--- configs/settings.py
+++ configs/settings.py
@@ -83,11 +83,12 @@ WSGI_APPLICATION = "configs.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
